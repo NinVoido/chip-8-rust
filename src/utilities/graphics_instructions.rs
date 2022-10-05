8c3761490fdc1ceb14371f7c4a735cb5197cdbe1
Your name <ninvoido.0@gmail.com>
2022-10-05 15:12:06 +0300

    Support for fonts added

---
+++ src/utilities/graphics_instructions.rs
@@ -0,0 +1,7 @@
+impl crate::utilities::cpu::Cpu{
+    ///FX29 instruction implementation
+    ///Sets index register to the address of hex character resembling value in register VX
+    fn choose_letter(mut self, x: u8){
+        self.i = 50+5*self.registers[x as usize] as u16
+    }
+}
--- src/utilities/mod.rs
+++ src/utilities/mod.rs
@@ -5,3 +5,4 @@ pub mod normal_instructions;
 pub mod arithmetic_instructions;
 pub mod timer_instructions;
 pub mod get_random;
+pub mod graphics_instructions;
